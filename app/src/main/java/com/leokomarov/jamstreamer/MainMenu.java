51a62f37df15b3731c99f0825f718112d354cce1
Nathan Douglas <nd9600@gmail.com>
2016-08-17 15:53:43 +0100

    Removed redundant methods

--- app/src/main/java/com/leokomarov/jamstreamer/MainMenu.java
+++ app/src/main/java/com/leokomarov/jamstreamer/MainMenu.java
@@ -16,6 +16,7 @@ import com.leokomarov.jamstreamer.common.TracksByName;
 import com.leokomarov.jamstreamer.common.TracksByNameAdapter;
 import com.leokomarov.jamstreamer.media_player.AudioPlayerService;
 import com.leokomarov.jamstreamer.playlist.PlaylistActivity;
+import com.leokomarov.jamstreamer.playlist.PlaylistAdapter;
 import com.leokomarov.jamstreamer.searches.AlbumsSearch;
 import com.leokomarov.jamstreamer.searches.ArtistsSearch;
 import com.leokomarov.jamstreamer.searches.TracksSearch;
@@ -26,7 +27,8 @@ public class MainMenu extends SherlockActivity {
 	private TextView textView_albums;
 	private TextView textView_tracks;
 	private TextView textView_topTracksThisWeek;
-	
+
+    //Stores whether you are going into the artists, albums, tracks, or top tracks this week activity
 	private void putHierarchy(String hierarchy){
 		SharedPreferences hierarchyPreference = getSharedPreferences(getString(R.string.hierarchyPreferences), 0);
     	SharedPreferences.Editor hierarchyEditor = hierarchyPreference.edit();
@@ -44,11 +46,13 @@ public class MainMenu extends SherlockActivity {
     	button_playlist.setOnClickListener(new View.OnClickListener() {
     		@Override
             public void onClick(View v) {
-                Intent button_playlistIntent = new Intent(getApplicationContext(), PlaylistActivity.class);
-                startActivityForResult(button_playlistIntent, 1);
+                Intent playlistIntent = new Intent(getApplicationContext(), PlaylistActivity.class);
+                startActivityForResult(playlistIntent, 1);
             }
     	});
-    	
+
+        //If the artists &c button is clicked
+        //put the appropriate string in the hierarchy, and start the activity
     	textView_artists = (TextView) findViewById(R.id.mainMenu_artists);    	
     	textView_artists.setOnClickListener(new View.OnClickListener() {
     		@Override
@@ -88,9 +92,10 @@ public class MainMenu extends SherlockActivity {
     			startActivityForResult(topTracksWeekIntent, 2);	
     		}
     	});
-    	
-    	//if FIRSTRUN_PREFERENCE doesn't contain "firstrun" or "firstrun" == false
-		// show the toasts
+
+
+    	//if the app hasn't been ran before, show the toasts
+		// ie FIRSTRUN_PREFERENCE doesn't contain "firstrun" or "firstrun" == true
         if (! getSharedPreferences("FIRSTRUN_PREFERENCE", MODE_PRIVATE).contains("firstrun") 
 				|| getSharedPreferences("FIRSTRUN_PREFERENCE", MODE_PRIVATE).getBoolean("firstrun", true)) {
         	Toast.makeText(getApplicationContext(),"Long-press on an album or track for options", Toast.LENGTH_LONG).show();
@@ -100,40 +105,24 @@ public class MainMenu extends SherlockActivity {
             firstrunEditor.putBoolean("firstrun", false);
             firstrunEditor.commit();
 		}   
-	}	
-	
-	public void artistsMainButton(View view){
-		putHierarchy("artists");
-		Intent artistIntent = new Intent(this, ArtistsSearch.class);
-		startActivity(artistIntent);
 	}
-	
-    public void albumsMainButton(View view){
-    	putHierarchy("albums");
-		Intent albumIntent = new Intent(this, AlbumsSearch.class);
-		startActivity(albumIntent);	
-	}
-    
-    public void tracksMainButton(View view){
-    	putHierarchy("tracks");
-		Intent trackIntent = new Intent(this, TracksSearch.class);
-		startActivity(trackIntent);	
-	}
-    
-    public void topTracksWeekMainButton(View view){
-    	putHierarchy("topTracksPerWeek");
-		Intent topTracksWeekIntent = new Intent(this, TracksByName.class);
-		startActivityForResult(topTracksWeekIntent, 2);	
-	}
-    
+
+    //Resets the checkboxes once you've left the playlist or top tracks activities
     @Override
 	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-	    if (requestCode == 2) {
-	    	TracksByNameAdapter.TracksByNameCheckboxList.clear();
-	    	TracksByNameAdapter.TracksByNameCheckboxCount = 0;
-	    }
+        switch (requestCode) {
+            case 1:
+                PlaylistAdapter.PlaylistCheckboxList.clear();
+                PlaylistAdapter.PlaylistCheckboxCount = 0;
+                break;
+            case 2:
+                TracksByNameAdapter.TracksByNameCheckboxList.clear();
+                TracksByNameAdapter.TracksByNameCheckboxCount = 0;
+                break;
+        }
 	}
-    
+
+    //Brings up the exit dialog when you press the back button
     @Override
     public void onBackPressed() {
         new AlertDialog.Builder(this)
@@ -148,7 +137,8 @@ public class MainMenu extends SherlockActivity {
         .setNegativeButton("No", null)
         .show();
     }
-    
+
+    //When you press the app home button, bring up the exit dialog
     public boolean onOptionsItemSelected(com.actionbarsherlock.view.MenuItem item) { 
 	        int itemId = item.getItemId();
 			if (itemId == android.R.id.home) {
